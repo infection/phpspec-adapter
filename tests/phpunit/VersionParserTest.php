ba100aa6dfd667bdba8c019c68e22c8826dab4d2
Théo FIDRY <theo.fidry@gmail.com>
2025-12-27 00:19:36 +0100

    test: Move the tests to match the expected location

--- tests/phpunit/VersionParserTest.php
+++ tests/phpunit/VersionParserTest.php
@@ -33,7 +33,7 @@
 
 declare(strict_types=1);
 
-namespace Infection\Tests\TestFramework\PhpSpec\Adapter;
+namespace Infection\Tests\TestFramework\PhpSpec;
 
 use Infection\TestFramework\PhpSpec\VersionParser;
 use InvalidArgumentException;
