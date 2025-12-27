5cfbe7df43ca35b1b086e1b667427976a2e5b089
Théo FIDRY <5175937+theofidry@users.noreply.github.com>
2025-12-27 19:06:34 +0100

    test: Add more tests for the VersionParser (#55)
    Using the [Semver
    specification](https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string),
    I added more tests.
    
    I preserved a few tests on top of those ones and I added the important,
    potentially unorthodox cases specific to the PhpSpec version.
    
    Despite a few cases that seems buggy, the ones about the PhpSpec version
    are all covered except `dev-main`, which I do not think is too big of a
    deal (at least it doesn't need to be addressed urgently).

--- tests/phpunit/VersionParserTest.php
+++ tests/phpunit/VersionParserTest.php
@@ -35,8 +35,10 @@ declare(strict_types=1);
 
 namespace Infection\Tests\TestFramework\PhpSpec;
 
+use Exception;
 use Infection\TestFramework\PhpSpec\VersionParser;
 use InvalidArgumentException;
+use function is_array;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
@@ -52,57 +54,283 @@ final class VersionParserTest extends TestCase
     }
 
     #[DataProvider('versionProvider')]
-    public function test_it_parses_version_from_string(string $content, string $expectedVersion): void
-    {
-        $result = $this->versionParser->parse($content);
+    public function test_it_parses_version_from_string(
+        string $value,
+        string|Exception $expected,
+    ): void {
+        if ($expected instanceof Exception) {
+            $this->expectExceptionObject($expected);
+        }
 
-        $this->assertSame($expectedVersion, $result);
-    }
+        $result = $this->versionParser->parse($value);
 
-    public function test_it_throws_exception_when_content_has_no_version_substring(): void
-    {
-        try {
-            $this->versionParser->parse('abc');
-
-            $this->fail();
-        } catch (InvalidArgumentException $exception) {
-            $this->assertSame(
-                'Parameter does not contain a valid SemVer (sub)string.',
-                $exception->getMessage(),
-            );
+        if (!($expected instanceof Exception)) {
+            $this->assertSame($expected, $result);
         }
     }
 
     public static function versionProvider(): iterable
     {
-        yield 'nominal stable' => ['7.0.2', '7.0.2'];
+        yield 'Hoa version' => ['3.17.05.02', '3.17.05'];
 
-        yield 'nominal development' => ['0.2.8', '0.2.8'];
+        yield 'common RC notation 1' => ['5.0.0-rc1', '5.0.0-rc1'];
 
-        yield 'stable variant' => ['v7.0.2', '7.0.2'];
+        yield 'common RC notation 2' => ['5.0.0-rc.1', '5.0.0-rc.1'];
 
-        yield 'development variant' => ['v0.2.8', '0.2.8'];
+        foreach (self::validSemanticVersionProvider() as $version) {
+            if (is_array($version)) {
+                yield $version;
+            } else {
+                yield [$version, $version];
 
-        yield 'patch' => ['7.0.2-patch', '7.0.2-patch'];
+                yield ['phpspec v' . $version . ' by Marcelo & Ciaran', $version];
+            }
+        }
 
-        yield 'versioned patch' => ['7.0.2-patch.0', '7.0.2-patch.0'];
+        foreach (self::invalidSemanticVersionProvider() as $version) {
+            yield is_array($version)
+                ? $version
+                : [$version, $version];
+        }
 
-        yield 'RC' => ['7.0.2-rc', '7.0.2-rc'];
+        foreach (self::phpSpecVersionProvider() as $version) {
+            if (is_array($version)) {
+                yield $version;
+            } else {
+                yield [$version, $version];
 
-        yield 'uppercase RC' => ['7.0.2-RC', '7.0.2-RC'];
+                yield ['phpspec v' . $version . ' by Marcelo & Ciaran', $version];
+            }
+        }
+    }
 
-        yield 'versioned RC' => ['7.0.2-rc.0', '7.0.2-rc.0'];
+    /**
+     * @return iterable<string|array{string, string|Exception}>
+     */
+    private static function validSemanticVersionProvider(): iterable
+    {
+        // Source: https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+        yield '0.0.4';
 
-        yield 'with spaces' => [' 7.0.2 ', '7.0.2'];
+        yield '1.2.3';
 
-        yield 'nonsense suffix 0' => ['7.0.2foo', '7.0.2'];
+        yield '10.20.30';
 
-        yield 'nonsense suffix 1' => ['7.0.2-foo', '7.0.2-foo'];
+        yield '1.1.2-prerelease+meta';
 
-        yield 'Hoa' => ['3.17.05.02', '3.17.05'];
+        yield '1.1.2+meta';
 
-        yield 'phpspec stable' => ['phpspec 7.1.0', '7.1.0'];
+        yield ['1.1.2+meta-valid', '1.1.2+meta'];    // TODO: this is incorrect
 
-        yield 'phpspec RC' => ['phpspec version 5.0.0-rc1', '5.0.0-rc1'];
+        yield '1.0.0-alpha';
+
+        yield '1.0.0-beta';
+
+        yield '1.0.0-alpha.beta';
+
+        yield '1.0.0-alpha.beta.1';
+
+        yield '1.0.0-alpha.1';
+
+        yield '1.0.0-alpha0.valid';
+
+        yield '1.0.0-alpha.0valid';
+
+        yield ['1.0.0-alpha-a.b-c-somethinglong+build.1-aef.1-its-okay', '1.0.0-alpha'];    // TODO: this is incorrect
+
+        yield '1.0.0-rc.1+build.1';
+
+        yield '2.0.0-rc.1+build.123';
+
+        yield '1.2.3-beta';
+
+        yield ['10.2.3-DEV-SNAPSHOT', '10.2.3-DEV'];    // TODO: this is incorrect
+
+        yield ['1.2.3-SNAPSHOT-123', '1.2.3-SNAPSHOT'];    // TODO: this is incorrect
+
+        yield '1.0.0';
+
+        yield '2.0.0';
+
+        yield '1.1.7';
+
+        yield '2.0.0+build.1848';
+
+        yield '2.0.1-alpha.1227';
+
+        yield '1.0.0-alpha+beta';
+
+        yield ['1.2.3----RC-SNAPSHOT.12.9.1--.12+788', '1.2.3'];    // TODO: this is incorrect
+
+        yield ['1.2.3----R-S.12.9.1--.12+meta', '1.2.3'];    // TODO: this is incorrect
+
+        yield ['1.2.3----RC-SNAPSHOT.12.9.1--.12', '1.2.3'];
+
+        yield ['1.0.0+0.build.1-rc.10000aaa-kk-0.1', '1.0.0+0.build.1'];    // TODO: this is incorrect
+
+        yield '99999999999999999999999.999999999999999999.99999999999999999';
+
+        yield '1.0.0-0A.is.legal';
+    }
+
+    /**
+     * @return iterable<string|array{string, string|Exception}>
+     */
+    private static function invalidSemanticVersionProvider(): iterable
+    {
+        // https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+        yield [
+            '1',
+            new InvalidArgumentException('Parameter does not contain a valid SemVer (sub)string.'),
+        ];
+
+        yield '1.2';
+
+        yield '1.2.3-0123';
+
+        yield '1.2.3-0123.0123';
+
+        yield '1.1.2+.123';
+
+        yield [
+            '+invalid',
+            new InvalidArgumentException('Parameter does not contain a valid SemVer (sub)string.'),
+        ];
+
+        yield [
+            '-invalid',
+            new InvalidArgumentException('Parameter does not contain a valid SemVer (sub)string.'),
+        ];
+
+        yield [
+            '-invalid+invalid',
+            new InvalidArgumentException('Parameter does not contain a valid SemVer (sub)string.'),
+        ];
+
+        yield [
+            '-invalid.01',
+            new InvalidArgumentException('Parameter does not contain a valid SemVer (sub)string.'),
+        ];
+
+        yield [
+            'alpha',
+            new InvalidArgumentException('Parameter does not contain a valid SemVer (sub)string.'),
+        ];
+
+        yield [
+            'alpha.beta',
+            new InvalidArgumentException('Parameter does not contain a valid SemVer (sub)string.'),
+        ];
+
+        yield [
+            'alpha.beta.1',
+            new InvalidArgumentException('Parameter does not contain a valid SemVer (sub)string.'),
+        ];
+
+        yield [
+            'alpha.1',
+            new InvalidArgumentException('Parameter does not contain a valid SemVer (sub)string.'),
+        ];
+
+        yield [
+            'alpha+beta',
+            new InvalidArgumentException('Parameter does not contain a valid SemVer (sub)string.'),
+        ];
+
+        yield [
+            'alpha_beta',
+            new InvalidArgumentException('Parameter does not contain a valid SemVer (sub)string.'),
+        ];
+
+        yield [
+            'alpha.',
+            new InvalidArgumentException('Parameter does not contain a valid SemVer (sub)string.'),
+        ];
+
+        yield [
+            'alpha..',
+            new InvalidArgumentException('Parameter does not contain a valid SemVer (sub)string.'),
+        ];
+
+        yield [
+            'beta',
+            new InvalidArgumentException('Parameter does not contain a valid SemVer (sub)string.'),
+        ];
+
+        yield ['1.0.0-alpha_beta', '1.0.0-alpha'];    // TODO: this is incorrect
+
+        yield [
+            '-alpha.',
+            new InvalidArgumentException('Parameter does not contain a valid SemVer (sub)string.'),
+        ];
+
+        yield '1.0.0-alpha..';
+
+        yield '1.0.0-alpha..1';
+
+        yield '1.0.0-alpha...1';
+
+        yield '1.0.0-alpha....1';
+
+        yield '1.0.0-alpha.....1';
+
+        yield '1.0.0-alpha......1';
+
+        yield '1.0.0-alpha.......1';
+
+        yield '01.1.1';
+
+        yield '1.01.1';
+
+        yield '1.1.01';
+
+        yield '1.2';
+
+        yield ['1.2.3.DEV', '1.2.3'];  // TODO: this is incorrect
+
+        yield '1.2-SNAPSHOT';
+
+        yield ['1.2.31.2.3----RC-SNAPSHOT.12.09.1--..12+788', '1.2.31'];  // TODO: this is incorrect
+
+        yield ['1.2-RC-SNAPSHOT', '1.2-RC'];  // TODO: this is incorrect
+
+        yield ['-1.0.3-gamma+b7718', '1.0.3-gamma+b7718'];  // TODO: this is incorrect
+
+        yield [
+            '+justmeta',
+            new InvalidArgumentException('Parameter does not contain a valid SemVer (sub)string.'),
+        ];
+
+        yield ['9.8.7+meta+meta', '9.8.7+meta'];  // TODO: this is incorrect
+
+        yield ['9.8.7-whatever+meta+meta', '9.8.7-whatever+meta'];  // TODO: this is incorrect
+
+        // TODO: this is incorrect
+        yield [
+            '99999999999999999999999.999999999999999999.99999999999999999----RC-SNAPSHOT.12.09.1--------------------------------..12',
+            '99999999999999999999999.999999999999999999.99999999999999999',
+        ];
+    }
+
+    /**
+     * @return iterable<string|array{string, string|Exception}>
+     */
+    private static function phpSpecVersionProvider(): iterable
+    {
+        // https://packagist.org/packages/phpspec/phpspec
+        yield '1.4.0';
+
+        yield '2.0.0-BETA1';
+
+        yield '2.0.0-RC1';
+
+        yield '2.2.0-BETA';
+
+        yield ['2.5.x-dev', '2.5.'];    // TODO: this is incorrect
+
+        yield [
+            'dev-main',
+            new InvalidArgumentException('Parameter does not contain a valid SemVer (sub)string.'),
+        ];    // TODO: this is incorrect
     }
 }
